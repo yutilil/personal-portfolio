73d97bc82a8426f585b490322134960ef99b6e0e
yutilil <yutilil@gmail.com>
2024-06-27 23:25:59 +0900

    rename

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# projects/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from projects.models import Project
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects' : projects
+    }
+    return render(request, 'projects/project_index.html', context)
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project' : project
+    }
+    return render(request, 'projects/project_detail.html', context)
