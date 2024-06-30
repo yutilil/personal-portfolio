c86ef1b44dbdeee4194a921297d4dbc5c620c885
yutilil <yutilil@gmail.com>
2024-06-30 15:50:55 +0900

    add images

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# projects/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from projects.models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -6,3 +6,4 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=100)
+    image = models.FileField(upload_to='project_images/', blank=True)
