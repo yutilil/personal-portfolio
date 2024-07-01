1bca3b65f981772b419723a0f769ea95c572e80c
yutilil <yutilil@gmail.com>
2024-07-02 07:58:10 +0900

    mod model

--- projects/models.py
+++ projects/models.py
@@ -7,3 +7,6 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=100)
     image = models.FileField(upload_to='project_images/', blank=True)
+
+    def __str__(self):
+        return self.title
